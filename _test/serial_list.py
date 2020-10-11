e974bf0f0fb92e0a8d05ddcd11ce42b80bf708c4
Norbert.Schwarz <info@datentechnikschwarz.de>
2020-10-11 12:15:49 +0200

    Init

--- _test/serial_list.py
+++ _test/serial_list.py
@@ -1,11 +1,28 @@
 import serial.tools.list_ports
 
-ports = serial.tools.list_ports.comports()
 
-if ports:
-    for p in ports:
-        com_port = p.device
-        com_desc = p.description
-        print(p.device, p.description)
-    print(len(ports), 'ports found')
+def list_comports(search_port):
+    ports = serial.tools.list_ports.comports()
 
+    if ports:
+        for p in ports:
+            if search_port in p.description:
+                # print(p.device)
+                return p.device
+        # print(len(ports), 'ports found')
+
+
+def main():
+
+    search_pattern = "CH340"
+
+    result = list_comports(search_pattern)
+
+    if result:
+        print(result)
+    else:
+        print("No Com-Port found")
+
+
+if __name__ == '__main__':
+    main()
