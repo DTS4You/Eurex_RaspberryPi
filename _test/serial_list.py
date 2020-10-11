adfceaa90d919bc06571ca3acf2796005108404f
Norbert.Schwarz <info@datentechnikschwarz.de>
2020-10-11 11:36:50 +0200

    Init

--- _test/serial_list.py
+++ _test/serial_list.py
@@ -2,6 +2,10 @@ import serial.tools.list_ports
 
 ports = serial.tools.list_ports.comports()
 
-for p in ports:
-    print(p.device, p.description)
-print(len(ports), 'ports found')
+if ports:
+    for p in ports:
+        com_port = p.device
+        com_desc = p.description
+        print(p.device, p.description)
+    print(len(ports), 'ports found')
+
