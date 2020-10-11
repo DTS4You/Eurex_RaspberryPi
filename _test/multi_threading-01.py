a66f8d740ed7a78a130f2f6c89e86c0aea4c4bc6
Norbert Schwarz <info@datentechnikschwarz.de>
2020-10-11 13:19:22 +0200

    Init

--- _test/multi_threading-01.py
+++ _test/multi_threading-01.py
@@ -3,13 +3,21 @@ from threading import Thread
 
 
 def sleeper(i):
-    print("thread %d sleeps for 5 seconds" % i)
+    print("thread {} sleeps for {} seconds".format(i,i))
     time.sleep(i)
     print("thread %d woke up" % i)
 
 
-for i in range(10):
-    t = Thread(target=sleeper, args=(i,))
-    t.start()
+def main():
+
+    for i in range(10):
+        t = Thread(target=sleeper, args=(i,))
+        t.start()
 
 
+###############################################################################
+# Main
+###############################################################################
+
+if __name__ == '__main__':
+    main()
